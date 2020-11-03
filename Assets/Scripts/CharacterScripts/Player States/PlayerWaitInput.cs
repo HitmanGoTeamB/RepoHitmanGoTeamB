e02eb3802b08dfca8ac03d580ffe3daf27277d1f
redd096 <martin.babino96@gmail.com>
2020-11-03 17:43:06 +0100

    movimento player da mouse e touch

--- Assets/Scripts/CharacterScripts/Player States/PlayerWaitInput.cs
+++ Assets/Scripts/CharacterScripts/Player States/PlayerWaitInput.cs
@@ -10,48 +10,121 @@ public class PlayerWaitInput : State
 
     Dictionary<Vector2Int, Waypoint> waypointsAround = new Dictionary<Vector2Int, Waypoint>();
 
+    Camera cam;
+    bool isMoving;
+    Vector2 startInputPosition;
+
     public override void Enter()
     {
         Player player = stateMachine as Player;
 
         //fill waypoints around me
         player.GetAllWaypointsAroundMe(waypointsAround);
+
+        //get camera reference
+        cam = Camera.main;
     }
 
     public override void Execution()
     {
-        IMovable objectToMove = null;
-        Waypoint waypointToMove = null;
+        //check on click and on release (touch or mouse)
+#if UNITY_ANDROID
+        if (Input.touchCount <= 0)
+            return;
 
-        //wait input, then set waypoint
-        if(Input.GetKeyDown(KeyCode.W))
+        Touch touch = Input.GetTouch(0);
+
+        if(!isMoving && touch.phase == TouchPhase.Began)
         {
-            objectToMove = stateMachine.GetComponent<IMovable>();
-
-            waypointToMove = objectToMove.GetWaypointToMove(waypointsAround[Vector2Int.up], false);
-            
+            OnClick();
         }
-        else if(Input.GetKeyDown(KeyCode.S))
+        else if(isMoving && (touch.phase == TouchPhase.Canceled || touch.phase == TouchPhase.Ended))
         {
-            objectToMove = stateMachine.GetComponent<IMovable>();
-
-            waypointToMove = objectToMove.GetWaypointToMove(waypointsAround[Vector2Int.down], false);
+            OnRelease();
         }
-        else if (Input.GetKeyDown(KeyCode.A))
+#else
+        if (!isMoving && Input.GetKeyDown(KeyCode.Mouse0))
         {
-            objectToMove = stateMachine.GetComponent<IMovable>();
-
-            waypointToMove = objectToMove.GetWaypointToMove(waypointsAround[Vector2Int.left], false);
+            OnClick();
         }
-        else if(Input.GetKeyDown(KeyCode.D))
+        else if(isMoving && Input.GetKeyUp(KeyCode.Mouse0))
         {
-            objectToMove = stateMachine.GetComponent<IMovable>();
-
-            waypointToMove = objectToMove.GetWaypointToMove(waypointsAround[Vector2Int.right], false);
+            OnRelease();
         }
+#endif
+    }
+
+#region private API
+
+    Vector2 GetInput()
+    {
+        //return touch position or mouse position
+#if UNITY_ANDROID
+        return Input.GetTouch(0).position;
+#else
+        return Input.mousePosition;
+#endif
+    }
+
+    void OnClick()
+    {
+        Vector2 inputPosition = GetInput();
+        Ray ray = cam.ScreenPointToRay(inputPosition);
+        int layer = CreateLayer.LayerOnly("Player");
+
+        //if hit player, save start input position and start moving
+        if (Physics.Raycast(ray, 100, layer))
+        {
+            startInputPosition = inputPosition;
+            isMoving = true;
+            Debug.Log(inputPosition);
+        }
+    }
+
+    void OnRelease()
+    {
+        //stop moving
+        isMoving = false;
+
+        Vector2 inputPosition = GetInput();
+        Ray ray = cam.ScreenPointToRay(inputPosition);
+        int layer = CreateLayer.LayerOnly("Player");
+
+        //be sure doesn't hit player again (no movement)
+        if (Physics.Raycast(ray, 100, layer))
+            return;
+
+        Vector2 movement = inputPosition - startInputPosition;
+        Debug.Log(inputPosition);
+        Debug.Log(movement);
+
+        //move on y axis
+        if (Mathf.Abs(movement.y) > Mathf.Abs(movement.x))
+        {
+            if (movement.y > 0)
+                Move(new Vector2Int(0, 1));     //move up
+            else
+                Move(new Vector2Int(0, -1));    //move down
+        }
+        //move on x axis
+        else
+        {
+            if (movement.x > 0)
+                Move(new Vector2Int(1, 0));     //move right
+            else
+                Move(new Vector2Int(-1, 0));    //move left
+        }
+    }
+
+#endregion
+
+    void Move(Vector2Int direction)
+    {
+        IMovable objectToMove = stateMachine.GetComponent<IMovable>();
+        Waypoint waypointToMove = objectToMove.GetWaypointToMove(waypointsAround[direction], false);
 
         //if there is a waypoint, change state to movement
-        if(waypointToMove != null)
+        if (waypointToMove != null)
         {
             stateMachine.SetState(new PlayerMovement(stateMachine, objectToMove, waypointToMove));
         }
