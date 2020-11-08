4f7700cdca1b064111b6e5f45f910201787b57cd
redd096 <martin.babino96@gmail.com>
2020-11-08 17:55:10 +0100

    aggiunto pulsar sui rock point + rimesso rotator sull'end point

---
+++ Assets/Scripts/Utility/Pulsar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Hitman GO/Utility/Pulsar")]
+public class Pulsar : MonoBehaviour
+{
+    [SerializeField] AnimationCurve curve = default;
+
+    void Update()
+    {
+        float size = curve.Evaluate(Time.time);
+        transform.localScale = new Vector3(size, size, size);
+    }
+}
