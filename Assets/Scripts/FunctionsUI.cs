49fa1607bc9bf080657df7f35b58d5117e33ef6e
redd096 <martin.babino96@gmail.com>
2020-11-17 12:19:40 +0100

    una sfilza di funzioni

--- Assets/Scripts/FunctionsUI.cs
+++ Assets/Scripts/FunctionsUI.cs
@@ -8,6 +8,8 @@ public class FunctionsUI : MonoBehaviour
 {
     public static FunctionsUI instance { get; private set; }
 
+    Resolution newResolution;
+
     void Awake()
     {
         //singleton
@@ -39,6 +41,58 @@ public class FunctionsUI : MonoBehaviour
 
     public void ChangeQuality()
     {
+        int currentQuality = QualitySettings.GetQualityLevel();
 
+        //set quality settings
+        QualitySettings.SetQualityLevel(currentQuality + 1, true);
+        int newQuality = currentQuality + 1;
+
+        //if reached limit (no change in quality), set quality to 0
+        if (QualitySettings.GetQualityLevel() == currentQuality)
+        {
+            QualitySettings.SetQualityLevel(0, true);
+            newQuality = 0;
+        }
+
+        //TODO deve cambiare il text della UI
+        Debug.Log(QualitySettings.names[newQuality]);
+    }
+
+    public void ChangeResolution()
+    {
+        Resolution currentResolution = Screen.currentResolution;
+
+        for(int i = 0; i < Screen.resolutions.Length; i++)
+        {
+            //find current resolution in the list
+            if(Screen.resolutions[i].width == currentResolution.width && Screen.resolutions[i].height == currentResolution.height)
+            {
+                //get next resolution or the back to 0
+                int nextResolution = i < Screen.resolutions.Length - 1 ? i + 1 : 0;
+                newResolution = Screen.resolutions[nextResolution];
+            }
+        }
+
+        //TODO deve cambiare il text della UI
+        Debug.Log(newResolution.width + " x " + newResolution.height + ", " + newResolution.refreshRate + "Hz");
+    }
+
+    public void SetResolution()
+    {
+        Screen.SetResolution(newResolution.width, newResolution.height, Screen.fullScreen, newResolution.refreshRate);
+    }
+
+    public void SetFullScreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+
+        Debug.Log(Screen.fullScreen ? "FULLSCREEN" : "WINDOW MODE");
+    }
+
+    public void SetVSync()
+    {
+        QualitySettings.vSyncCount = QualitySettings.vSyncCount <= 0 ? 1 : 0;
+
+        Debug.Log(QualitySettings.vSyncCount <= 0 ? "V SYNC OFF" : "V SYNC ON");
     }
 }
